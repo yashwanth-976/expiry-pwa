8a2aabf97a0c27a864fa985dcead17fde866e2fc
yashwanth-976 <yashwanthkoppada@gmail.com>
2025-12-20 19:34:48 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,39 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = "expiryguard-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("expiry-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "style.css",
-        "app.js"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
